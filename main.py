e4777c279e11329cc6000c01389ff36df326bdfd
AZIMJON <129381353+theazimjo@users.noreply.github.com>
2026-09-05 16:57:46 +0500

    yangi maxsulot qidirish tizimi qo'shildi

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
